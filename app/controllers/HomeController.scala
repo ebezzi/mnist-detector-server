e34d529a09b83ebb2901919a9ac7a3b1fec659fb
Emanuele Bezzi <emanuele.bezzi@databiz.it>
2016-05-12 21:18:42 +0200

    it works

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,7 +1,18 @@
 package controllers
 
+import java.io.{DataInputStream, FileInputStream}
 import javax.inject._
+import org.deeplearning4j.eval.Evaluation
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork
+import org.nd4j.linalg.api.ndarray.{BaseNDArray, INDArray}
+import org.nd4j.linalg.cpu.NDArray
+import org.nd4j.linalg.dataset.DataSet
+import org.nd4j.linalg.factory.Nd4j
+import java.io.File
+
 import play.api._
+import play.api.libs.json.Json
 import play.api.mvc._
 
 /**
@@ -18,7 +29,63 @@ class HomeController @Inject() extends Controller {
    * a path of `/`.
    */
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index())
+  }
+
+  def detect() = Action { req =>
+
+    val input = req.body.asJson.get.as[Seq[Double]].toArray.map(255-_)
+
+    println (input.length)
+
+    val dataDir = "/opt/spark-deep-learning-app/src/main/resources/convnet/"
+
+    val conf = scala.io.Source.fromFile(new File(dataDir + "conf.json")).mkString
+
+    //Load network configuration from disk:
+    val confFromJson = MultiLayerConfiguration.fromJson((conf))
+
+    //Load parameters from disk:;
+    val dis = new DataInputStream(new FileInputStream(dataDir + "coefficients.bin"))
+    val newParams = Nd4j.read(dis)
+
+    //Create a MultiLayerNetwork from the saved configuration and parameters
+    val savedNetwork = new MultiLayerNetwork(confFromJson)
+    savedNetwork.init()
+    savedNetwork.setParameters(newParams)
+
+//    val m = input.sliding(28,28).map(_.toArray).toArray
+
+//    println(m)
+
+    val xs = new NDArray(input, Array(1, 784), 'c')
+//    val xs = new NDArray(m)
+
+//    println(xs)
+
+    val output = savedNetwork.output(xs)
+
+//
+//    val eval = new Evaluation()
+//    val ds = new DataSet()
+//    ds.setFeatures()
+//    val output = savedNetwork.output(ds.getFeatureMatrix)
+//    eval.eval(ds.getLabels, output)
+//    println(eval.stats())
+//
+    val v = (0 to 9).map(output.getInt(_)).toList
+//
+//    println(v.indexOf(1))
+//
+//    Ok(v.indexOf(1).toString)
+
+    val res = Json.obj("results" -> Json.arr(
+      v, v
+    ))
+
+    Ok(res)
+
+
   }
 
 }
--- build.sbt
+++ build.sbt
@@ -13,4 +13,10 @@ libraryDependencies ++= Seq(
   "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.1" % Test
 )
 
+libraryDependencies += "org.nd4j" % "nd4j-x86" % "0.4-rc3.8"
+
+libraryDependencies += "org.deeplearning4j" % "dl4j-spark" % "0.4-rc3.8"
+
+libraryDependencies += "org.nd4j" % "nd4j-api" % "0.4-rc3.8"
+
 resolvers += "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
